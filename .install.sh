d3e887cdd2738a653ad4ba76ab6e3f43ed28286e
volkovasystems <richeve.bebedor@gmail.com>
2017-04-09 20:02:57 +0800

    Finished x10d procedure.

---
+++ .install.sh
@@ -0,0 +1,20 @@
+yarn add \
+babel-runtime \
+falzy \
+harden \
+protype \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+falzy \
+harden \
+protype \
+--save \
+--force-latest
---
+++ x10d-test.js
@@ -0,0 +1,4 @@
+
+const x10d = require( "./x10d.js" );
+
+console.log(  require( "util" ).inspect( x10d( function Hello( ){ } ), { "showHidden": true } ) );

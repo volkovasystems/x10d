a8f16626c8fa6f3a04fbbf69d69e2d301cb1605c
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 13:15:05 +0800

    Updated test.

--- x10d-test.js
+++ x10d-test.js
@@ -1,4 +1,6 @@
-
+const assert = require( "assert" );
 const x10d = require( "./x10d.js" );
 
-console.log(  require( "util" ).inspect( x10d( function Hello( ){ } ), { "showHidden": true } ) );
+assert.ok( x10d( function Hello( ){ } ) );
+
+console.log( "ok" );
